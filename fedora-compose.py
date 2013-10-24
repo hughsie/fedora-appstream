64259ad687baa646293ba642e6635e5d27adbd90
Richard Hughes <richard@hughsie.com>
2013-10-24 09:54:02 +0100

    Reassemble the XML file during compose in a smarter way

--- fedora-compose.py
+++ fedora-compose.py
@@ -28,6 +28,7 @@ import shutil
 import tarfile
 import gzip
 import datetime
+import xml.etree.ElementTree as ET
 
 import config
 from logger import Logger, LoggerItem
@@ -35,100 +36,104 @@ from logger import Logger, LoggerItem
 timestamp = datetime.datetime.now().strftime('%Y%m%d')
 sys.stdout = Logger("compose-%s.txt" % timestamp)
 
-def main():
+class Compose:
+
+    def __init__(self):
+        self.cfg = config.Config()
+        self.log = LoggerItem()
+        self.application_ids = {}
+
+        # used as a temp location
+        if os.path.exists('./tmp'):
+            shutil.rmtree('./tmp')
+        os.makedirs('./tmp')
+        if os.path.exists('./' + self.cfg.distro_name + '.xml.gz'):
+            os.remove('./' + self.cfg.distro_name + '.xml.gz')
+        if os.path.exists('./' + self.cfg.distro_name + '-icons.tar.gz'):
+            os.remove('./' + self.cfg.distro_name + '-icons.tar.gz')
+
+    def run(self):
+
+        files = glob.glob("./appstream/*.xml")
+        files.sort()
+
+        # setup the output XML
+        master_root = ET.Element("applications")
+        master_root.set("version", "0.1")
+        master_tree = ET.ElementTree(master_root)
+
+        recognised_types = ['desktop', 'codec', 'font', 'inputmethod']
+        for filename in files:
+            self.log.update_key(filename)
+            try:
+                tree = ET.parse(filename)
+            except ET.ParseError, e:
+                self.log.write(LoggerItem.WARNING, "XML could not be parsed: %s" % str(e))
+                continue
+            root = tree.getroot()
+            for app in root:
+                app_id = app.find('id')
+
+                # check type is known
+                app_id_type = app_id.get('type')
+                if app_id_type not in recognised_types:
+                    self.log.write(LoggerItem.WARNING,
+                              "appstream id type %s not recognised" % app_id_type)
+                    continue
+
+                # detect duplicate IDs in the data
+                if self.application_ids.has_key(app_id):
+                    found = self.application_ids[app_id.text]
+                    self.log.write(LoggerItem.WARNING,
+                              "duplicate ID found in %s and %s" % (filename, found))
+                    continue
+
+                # add everything that isn't private
+                new = ET.SubElement(master_root, 'application')
+                for elem in app:
+                    if elem.tag.startswith("X-"):
+                        continue
+                    new.append(elem)
+
+                # success
+                self.application_ids[app_id.text] = filename
+                self.log.write(LoggerItem.INFO, "adding %s" % app_id.text)
+
+        # write to compressed file
+        master = gzip.open('./' + self.cfg.distro_name + '.xml.gz', 'wb')
+        master_tree.write(master, 'UTF-8')
+        master.close()
+
+        # we have to do this as "tar --concatenate" is broken
+        files = glob.glob("./appstream/*.tar")
+        files.sort()
+        for f in files:
+            tar = tarfile.open(f, "r")
+            tar.extractall(path='./tmp')
+            tar.close()
+
+        # create master icons file
+        tar = tarfile.open('./' + self.cfg.distro_name + '-icons.tar', "w")
+        files = glob.glob("./tmp/*.png")
+        for f in files:
+            tar.add(f, arcname=f.split('/')[-1])
+        tar.close()
+
+        # compress to save a few Mb
+        f_in = open('./' + self.cfg.distro_name + '-icons.tar', 'rb')
+        f_out = gzip.open('./' + self.cfg.distro_name + '-icons.tar.gz', 'wb')
+        f_out.writelines(f_in)
+        f_out.close()
+        f_in.close()
+        os.remove('./' + self.cfg.distro_name + '-icons.tar')
+
+if __name__ == "__main__":
 
     # check we're not top level
     if os.path.exists('./application.py'):
         print 'You cannot run these tools from the top level directory'
         sys.exit(1)
 
-    cfg = config.Config()
-    log = LoggerItem()
-
-    application_ids = {}
-
-    # used as a temp location
-    if os.path.exists('./tmp'):
-        shutil.rmtree('./tmp')
-    os.makedirs('./tmp')
-    if os.path.exists('./' + cfg.distro_name + '.xml.gz'):
-        os.remove('./' + cfg.distro_name + '.xml.gz')
-    if os.path.exists('./' + cfg.distro_name + '-icons.tar.gz'):
-        os.remove('./' + cfg.distro_name + '-icons.tar.gz')
-
-    files = glob.glob("./appstream/*.xml")
-    files.sort()
-
-    master = gzip.open('./' + cfg.distro_name + '.xml.gz', 'wb')
-    master.write('<?xml version="1.0"?>\n')
-    master.write('<applications version="0.1">\n')
-    for filename in files:
-        f = open(filename, 'r')
-        s = f.read()
-
-        # detect duplicate IDs in the data
-        is_dupe = False
-        for l in s.split('\n'):
-            if l.startswith('    <id '):
-                if l.startswith('    <id type="desktop">'):
-                    app_id = l[23:-5]
-                elif l.startswith('    <id type="codec">'):
-                    app_id = l[21:-5]
-                elif l.startswith('    <id type="font">'):
-                    app_id = l[20:-5]
-                elif l.startswith('    <id type="inputmethod">'):
-                    app_id = l[27:-5]
-                else:
-                    log.update_key(filename)
-                    log.write(LoggerItem.WARNING,
-                              "appstream id type %s not recognised" % l)
-                    break
-                log.update_key(app_id)
-                if application_ids.has_key(app_id):
-                    found = application_ids[app_id]
-                    is_dupe = True
-                    log.write(LoggerItem.WARNING,
-                              "duplicate ID found in %s and %s" % (f, found))
-                application_ids[app_id] = f
-        if is_dupe:
-            continue
-
-        # TODO: such a hack, but it's so quick....
-        s = s.replace('<?xml version="1.0"?>\n', '')
-        s = s.replace('<applications version="0.1">\n', '')
-        s = s.replace('</applications>\n', '')
-        f.close()
-
-        log.update_key(filename)
-        log.write(LoggerItem.INFO, "adding contents")
-
-        master.write(s)
-    master.write('</applications>\n')
-    master.close()
-
-    # we have to do this as "tar --concatenate" is broken
-    files = glob.glob("./appstream/*.tar")
-    files.sort()
-    for f in files:
-        tar = tarfile.open(f, "r")
-        tar.extractall(path='./tmp')
-        tar.close()
-
-    # create master icons file
-    tar = tarfile.open('./' + cfg.distro_name + '-icons.tar', "w")
-    files = glob.glob("./tmp/*.png")
-    for f in files:
-        tar.add(f, arcname=f.split('/')[-1])
-    tar.close()
-
-    # compress to save a few Mb
-    f_in = open('./' + cfg.distro_name + '-icons.tar', 'rb')
-    f_out = gzip.open('./' + cfg.distro_name + '-icons.tar.gz', 'wb')
-    f_out.writelines(f_in)
-    f_out.close()
-    f_in.close()
-    os.remove('./' + cfg.distro_name + '-icons.tar')
-
-if __name__ == "__main__":
-
-    main()
+    compose = Compose()
+    compose.run()
+    sys.exit(0)
