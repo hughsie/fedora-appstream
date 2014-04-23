bfadc31115fea9cb743f28d43d4eaa456d9ce973
Richard Hughes <richard@hughsie.com>
2014-04-23 13:10:01 +0100

    Remove ununsed data files

--- config.py
+++ config.py
@@ -33,8 +33,6 @@ class Config:
         self._config = GLib.KeyFile()
         self._config.load_from_file(filename, GLib.KeyFileFlags.NONE)
         self._group_name = u'fedora-appstream'
-        self.icon_size = self._config.get_integer(self._group_name, 'IconSize')
-        self.min_icon_size = self._config.get_integer(self._group_name, 'MinIconSize')
 
         # get the project defaults
         self.cfg_project = ConfigParser.ConfigParser()
@@ -43,42 +41,10 @@ class Config:
         self.distro_tag = self.cfg_project.get('AppstreamProject', 'DistroTag')
         self.repo_ids = self.cfg_project.get('AppstreamProject', 'RepoIds').split(',')
 
-    def get_id_blacklist(self):
-        blacklist = []
-        try:
-            blacklist = self._config.get_string_list(self._group_name, 'BlacklistIds')
-        except Exception as e:
-            pass
-        return blacklist
-
-    def get_content_licences(self):
-        licences = []
-        try:
-            licences = self._config.get_string_list(self._group_name, 'AcceptableContentLicences')
-        except Exception as e:
-            pass
-        return licences
-
-    def get_category_blacklist(self):
-        blacklist = []
-        try:
-            blacklist = self._config.get_string_list(self._group_name, 'BlacklistCategories')
-        except Exception as e:
-            pass
-        return blacklist
-
     def get_package_blacklist(self):
         blacklist = []
         try:
             blacklist = self._config.get_string_list(self._group_name, 'BlacklistPackages')
-        except Exception as e:
-            pass
-        return blacklist
-
-    def get_category_ignore_list(self):
-        blacklist = []
-        try:
-            blacklist = self._config.get_string_list(self._group_name, 'IgnoreCategories')
         except Exception as e:
             pass
         return blacklist
@@ -91,50 +57,6 @@ class Config:
             pass
         return data
 
-    def get_screenshot_mirror_url(self):
-
-        data = []
-        try:
-            data = self.cfg_project.get('AppstreamProject', 'ScreenshotMirrorUrl')
-        except Exception as e:
-            pass
-        return data
-
-    def get_screenshot_thumbnail_sizes(self):
-        data = []
-        sizes = []
-        try:
-            data = self._config.get_string_list(self._group_name, 'ScreenshotThumbnailSizes')
-        except Exception as e:
-            pass
-        for d in data:
-            tmp = d.split('x')
-            sizes.append((int(tmp[0]), int(tmp[1])))
-        return sizes
-
-    def get_preferred_icon_sizes(self):
-        prefs = []
-        try:
-            prefs = self._config.get_string_list(self._group_name, 'PreferredIconSizes')
-        except Exception as e:
-            pass
-        return prefs
-
-    def get_int(self, key):
-        prefs = []
-        try:
-            prefs = self._config.get_integer(self._group_name, key)
-        except Exception as e:
-            pass
-        return prefs
-
-    def get_stock_icons(self):
-        # get the list of stock icons
-        f = open('../data/stock-icon-names.txt', 'r')
-        stock_icons = f.read().rstrip().split('\n')
-        f.close()
-        return stock_icons
-
     def get_package_data_list(self):
         # add any extra packages required
         common_packages = []
@@ -143,52 +65,3 @@ class Config:
                 tmp = k[12:-1]
                 common_packages.append((tmp, self._config.get_string(self._group_name, k)))
         return common_packages
-
-    def get_category_extra_list(self):
-        # add any extra categories required
-        categories_add = {}
-        for k in self._config.get_keys(self._group_name)[0]:
-            if k.startswith('CategoryAdd'):
-                tmp = k[12:-1]
-                categories_add[tmp] = self._config.get_string(self._group_name, k)
-        return categories_add
-
-    def get_category_extra_for_id(self, id):
-        # get additional categories for a specific application id
-        add = []
-        try:
-            add = self._config.get_string_list(self._group_name, 'CategoryAdd(' + id + ')')
-        except Exception as e:
-            pass
-        return add
-
-    def get_project_group_for_id(self, id):
-        # get project group for a specific application id
-        value = None
-        try:
-            value = self._config.get_string(self._group_name, 'ProjectGroup(' + id + ')')
-        except Exception as e:
-            pass
-        return value
-
-def main():
-    cfg = Config()
-    print 'distro-name:\t\t', cfg.distro_name
-    print 'icon-size:\t\t', cfg.icon_size
-    print 'min-icon-size:\t\t', cfg.min_icon_size
-    print 'screenshot-mirror-url:\t', cfg.get_screenshot_mirror_url()
-    print 'screenshot-thumbnail-sizes:\t', cfg.get_screenshot_thumbnail_sizes()
-    print 'preferred-sizes:\t', cfg.get_preferred_icon_sizes()
-    print 'interesting-installed-files:\t', cfg.get_interesting_installed_files()
-    print 'content-licences:\t', cfg.get_content_licences()
-    print 'ignore-categories:\t', cfg.get_category_ignore_list()
-    print 'blacklist-categories:\t', cfg.get_category_blacklist()
-    print 'blacklist-packages:\t', cfg.get_package_blacklist()
-    print 'common-packages:\t', len(cfg.get_package_data_list())
-    print 'blacklist-ids:\t\t', len(cfg.get_id_blacklist())
-    print 'stock-icons:\t\t', len(cfg.get_stock_icons())
-    print 'categories-add:\t\t', len(cfg.get_category_extra_list())
-    sys.exit(0)
-
-if __name__ == "__main__":
-    main()
